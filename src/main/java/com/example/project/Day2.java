68c688bc30e539a89023dcf9e731f6b307a8d54c
brianw3943 <brianw124@nycstudents.net>
2024-12-17 17:06:20 +0000

    commit

--- src/main/java/com/example/project/Day2.java
+++ src/main/java/com/example/project/Day2.java
@@ -1,10 +1,24 @@
 
 package com.example.project;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Day2{
     public static String[][] nameSort(String[] names){ // your will be tested on this method
-        return null; //you must return a two dimensional string array
+        String[] naughty = new String[names.length];
+        String[] nice = new String[names.length];
+        int random;
+        for (int i = 0; i < names.length; i++) {
+            random = (int) (Math.random() * 2);
+            if (random == 0) {
+                naughty[i] = names[i];
+            } else {
+                nice[i] = names[i];
+            }
+        }
+        String[][] output = {nice, naughty};
+        return output; //you must return a two dimensional string array
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/project/Test.java
+++ src/main/java/com/example/project/Test.java
@@ -1,8 +1,20 @@
 package com.example.project;
 public class Test {
     public static void main(String args[]) {
-        String name = "";
-        String elfName = Day1.generateElfName(name);
-        System.out.println(elfName);
+        String[] names = {"John", "Matt", "Lucy", "Nina", "Roxy", "Andrew", "Henry", "Donnie", "Sasha", "Mohammad", "Elsa", "Ruby"};
+        String[][] result = Day2.nameSort(names);
+        System.out.println(result[0][0]);
+        System.out.println(result[0][1]);
+        System.out.println(result[0][2]);
+        System.out.println(result[0][3]);
+        System.out.println(result[0][4]);
+        System.out.println(result[0][5]);
+        System.out.println(result[1][0]);
+        System.out.println(result[1][1]);
+        System.out.println(result[1][2]);
+        System.out.println(result[1][3]);
+        System.out.println(result[1][4]);
+        System.out.println(result[1][5]);
+
     }
 }
