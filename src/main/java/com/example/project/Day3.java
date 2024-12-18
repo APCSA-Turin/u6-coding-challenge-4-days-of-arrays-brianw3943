095127aa8a99d42d83ea5054bcfae2b9a47ff202
brianw3943 <brianw124@nycstudents.net>
2024-12-18 16:42:02 +0000

    commit

--- src/main/java/com/example/project/Day3.java
+++ src/main/java/com/example/project/Day3.java
@@ -3,9 +3,21 @@ package com.example.project;
 public class Day3 {
       public static String[][] generateSnowflake(int size) { //you will be tested on this method
         String[][] grid = new String[size][size];
-
-    
-
+        for (int a = 0; a < size; a++) {
+          for (int b = 0; b < size; b++) {
+            grid[a][b] = " ";
+          }
+        } 
+        for (int i = 0; i < size; i++) {
+          grid[i][i] = "*";
+        }
+        for (int j = 0; j < size; j++) {
+          grid[j][size - 1 - j] = "*";
+        }
+        for (int k = 0; k < size; k++) {
+          grid[(size/2)][k] = "*";
+          grid[k][(size/2)] = "*";
+        }
         return grid;
     }
 
@@ -16,5 +28,13 @@ public class Day3 {
 
     // Test for the snowflake generation
     public static void main(String[] args) {
+      int size = 7; // Small odd size
+      String[][] snowflake = Day3.generateSnowflake(size);
+      System.out.println(snowflake);
+      for (int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++)
+            System.out.print(snowflake[i][j]);
+          System.out.println();
+      }
     }
 }
